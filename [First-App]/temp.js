57aadc583444a710d9ad6080a483ec36d3adbdcf
Fevziurtn <fevziurten@gmail.com>
2023-01-09 18:48:01 +0300

    Projeler

---
+++ [First-App]/temp.js
@@ -0,0 +1,12 @@
+const categories = {
+    resource: {
+        label:"Malzemeler",
+        icon: "",
+    },
+    food: {
+        label:"Besinler",
+    },
+    tool:{
+        label:"Araçlar",
+    },
+}
